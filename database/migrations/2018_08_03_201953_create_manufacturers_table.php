83cdaca2a5f0253a5d194b2573f7e96607257695
jedwards <jon.edwards@live.co.uk>
2018-08-03 22:05:12 +0100

    [ADD] Database design migration
    Added migration files to create the following tables:
    vehicles
    models
    manufacturers
    owners
    colours

---
+++ database/migrations/2018_08_03_201953_create_manufacturers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateManufacturersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('manufacturer', 10)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manufacturers');
+    }
+}

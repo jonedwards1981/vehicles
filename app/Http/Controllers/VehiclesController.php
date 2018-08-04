b2055f021d421329bdc0756f8a99857ce4f045bd
jedwards <jon.edwards@live.co.uk>
2018-08-04 22:18:37 +0100

    [UPDATE] display all columns of vehicle table

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -18,14 +18,30 @@ class VehiclesController extends Controller
         $data = [];
         
         $vehicles = Vehicle::all();
+        
+        $data['vehicles'] = $vehicles;
 
-        foreach ($vehicles as $vehicle) {
+        /*foreach ($vehicles as $vehicle) {
             $data['vehicles'][] = ['model_id' => $vehicle->model_id,
-                       'manufacturer_id' => $vehicle->model_id,
-                       'type' => $vehicle->type,
-                       'usage' => $vehicle->usage,
-                       'license_plate' => $vehicle->license_plate,];
-        }
+                        'manufacturer_id' => $vehicle->manufacturer_id,
+                        'type' => $vehicle->type,
+                        'usage' => $vehicle->usage,
+                        'license_plate' => $vehicle->license_plate,
+                        'weight_category' =>$vehicle->weight_category,
+                        'no_seats' => $vehicle->no_seats,
+                        'has_boot' => $vehicle->has_boot,
+                        'has_trailer' =>$vehicle->has_trailer,
+                        'owner_id' => $vehicle->owner_id,
+                        'transmission' => $vehicle->transmission,
+                        'colour_id' => $vehicle->colour_id,
+                        'is_hgv' => $vehicle->is_hgv,
+                        'no_doors' => $vehicle->no_doors,
+                        'sunroof' => $vehicle->sunroof,
+                        'has_gps' => $vehicle->has_ggps,
+                        'no_wheels' => $vehicle->no_wheels,
+                        'engine_cc' => $vehicle->engine_cc,
+                        'fuel_type' => $vehicle->fuel_type];
+        }*/
         
         
         return view('vehicles', $data);
--- resources/views/vehicles.blade.php
+++ resources/views/vehicles.blade.php
@@ -15,9 +15,52 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
+                    <table border="1">
+                        <tr>
+                            <th>model_id</th>
+                            <th>manufacturer_id</th>
+                            <th>type</th>
+                            <th>usage</th>
+                            <th>license_plate</th>
+                            <th>weight_category</th>
+                            <th>no_seats</th>
+                            <th>has_boot</th>
+                            <th>has_trailer</th>
+                            <th>owner_id</th>
+                            <th>transmission</th>
+                            <th>colour_id</th>
+                            <th>is_hgv</th>
+                            <th>no_doors</th>
+                            <th>sunroof</th>
+                            <th>has_gps</th>
+                            <th>no_wheels</th>
+                            <th>engine_cc</th>
+                            <th>fuel_type</th>
+                        </tr>
                     @foreach ($vehicles as $vehicle)
-                    This is vehicle {{ $vehicle['license_plate'] }}<br>
+                        <tr>
+                            <td>{{$vehicle->model_id}}</td>
+                            <td>{{$vehicle->manufacturer_id}}</td>
+                            <td>{{$vehicle->type}}</td>
+                            <td>{{$vehicle->usage}}</td>
+                            <td>{{$vehicle->license_plate}}</td>
+                            <td>{{$vehicle->weight_category}}</td>
+                            <td>{{$vehicle->no_seats}}</td>
+                            <td>{{$vehicle->has_boot}}</td>
+                            <td>{{$vehicle->has_trailer}}</td>
+                            <td>{{$vehicle->owner_id}}</td>
+                            <td>{{$vehicle->transmission}}</td>
+                            <td>{{$vehicle->colour_id}}</td>
+                            <td>{{$vehicle->is_hgv}}</td>
+                            <td>{{$vehicle->no_doors}}</td>
+                            <td>{{$vehicle->sunroof}}</td>
+                            <td>{{$vehicle->has_gps}}</td>
+                            <td>{{$vehicle->no_wheels}}</td>
+                            <td>{{$vehicle->engine_cc}}</td>
+                            <td>{{$vehicle->fuel_type}}</td>
+                        </tr>
                     @endforeach
+                    </table>
                 </div>
             </div>
         </div>

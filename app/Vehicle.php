d8ba6a097dd605d1113b809cc290187ace7e1e22
jedwards <jon.edwards@live.co.uk>
2018-08-04 22:57:34 +0100

    [UPDATE] display manufacturer, owner, and colour details
    Define db relationships so that ids can be looked up
    
    --todo rename model due to reserved name

--- app/Vehicle.php
+++ app/Vehicle.php
@@ -8,5 +8,28 @@ class Vehicle extends Model
 {
     public $timestamps = false;
     
-    //
+    /**
+     * Get the manufacturer associated with the vehicle.
+     */
+    public function manufacturer()
+    {
+        return $this->belongsTo('App\Manufacturer');
+    }
+    
+    /**
+     * Get the owner associated with the vehicle.
+     */
+    public function owner()
+    {
+        return $this->belongsTo('App\Owner');
+    }
+    
+    /**
+     * Get the colour associated with the vehicle.
+     */
+    public function colour()
+    {
+        return $this->belongsTo('App\Colour');
+    }
+
 }
--- resources/views/vehicles.blade.php
+++ resources/views/vehicles.blade.php
@@ -18,7 +18,7 @@
                     <table border="1">
                         <tr>
                             <th>model_id</th>
-                            <th>manufacturer_id</th>
+                            <th>manufacturer</th>
                             <th>type</th>
                             <th>usage</th>
                             <th>license_plate</th>
@@ -26,9 +26,11 @@
                             <th>no_seats</th>
                             <th>has_boot</th>
                             <th>has_trailer</th>
-                            <th>owner_id</th>
+                            <th>owner_name</th>
+                            <th>owner_company</th>
+                            <th>owner_profession</th>
                             <th>transmission</th>
-                            <th>colour_id</th>
+                            <th>colour</th>
                             <th>is_hgv</th>
                             <th>no_doors</th>
                             <th>sunroof</th>
@@ -40,7 +42,7 @@
                     @foreach ($vehicles as $vehicle)
                         <tr>
                             <td>{{$vehicle->model_id}}</td>
-                            <td>{{$vehicle->manufacturer_id}}</td>
+                            <td>{{$vehicle->manufacturer->manufacturer}}</td>
                             <td>{{$vehicle->type}}</td>
                             <td>{{$vehicle->usage}}</td>
                             <td>{{$vehicle->license_plate}}</td>
@@ -48,9 +50,11 @@
                             <td>{{$vehicle->no_seats}}</td>
                             <td>{{$vehicle->has_boot}}</td>
                             <td>{{$vehicle->has_trailer}}</td>
-                            <td>{{$vehicle->owner_id}}</td>
+                            <td>{{$vehicle->owner->name}}</td>
+                            <td>{{$vehicle->owner->company}}</td>
+                            <td>{{$vehicle->owner->profession}}</td>
                             <td>{{$vehicle->transmission}}</td>
-                            <td>{{$vehicle->colour_id}}</td>
+                            <td>{{$vehicle->colour->colour}}</td>
                             <td>{{$vehicle->is_hgv}}</td>
                             <td>{{$vehicle->no_doors}}</td>
                             <td>{{$vehicle->sunroof}}</td>
